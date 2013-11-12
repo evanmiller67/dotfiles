e4bbf8920a456aa13ca2ecf5831529025bde6453
Evan Miller <evanmiller67@gmail.com>
2013-11-12 09:01:12 -0600

    honor CLI vars

--- utils/cache_warm.sh
+++ utils/cache_warm.sh
@@ -1,17 +1,38 @@
+#!/bin/bash
+
 # ensure EMAIL, PASSWORD, DOMAIN
-if [ -z "$DOMAIN" ] && [ -z "$EMAIL" ] && [ -z "$PASSWORD" ]
-then 
-  echo "Missing environment variables. Make sure the following are set: \$DOMAIN \$EMAIL \$PASSWORD"
+env=false
+arg=false
+if [ -z "$DOMAIN" ] && [ -z "$EMAIL" ] && [ -z "$PASSWORD" ]; then
+  env=true
+fi
+if [ ! $# == 3 ]; then
+  arg=true
+fi
+
+if $arg && $env; then
+  echo "$(basename "$0") [DOMAIN] [EMAIL] [PASSWORD] -- warm the cache for the client app using the domain and credentials supplied. May also be environment variables.
+
+where:
+    DOMAIN   - base URL to use (e.g. client.changehealthcare.com)
+    EMAIL    - email address used to login (e.g. cache_warm+all@changehealthcare.com
+    PASSWORD - password tied to email (e.g. @sdf1234)"
   exit 1;
 fi
 
-# create a new session
-# curl -s --cookie-jar cookie http://$ > /dev/null
-# echo "session setup"
+if $env; then
+  domain="$1"
+  email="$2"
+  pass="$3"
+else
+  domain="$DOMAIN"
+  email="$EMAIL"
+  pass="$PASSWORD"
+fi
 
 # set authenticity_token for credentials
-authToken=`curl -s --cookie-jar session "http://$DOMAIN" | grep csrf-token | awk '{print $2;}' | awk -F"\"" '{print $2;}'`
-if [ -z "$authToken" ] && [ -z "${authTOken+xxx}" = "xxx" ]
+authToken=`curl -s --cookie-jar session "http://$domain" | grep csrf-token | awk '{print $2;}' | awk -F"\"" '{print $2;}'`
+if [ -z "$authToken" ] && [ -z "${authToken+xxx}" = "xxx" ]
   then
   echo "Did not receive authenticity_token." 1>&2
   exit 1;
@@ -19,27 +40,29 @@ fi
 echo "authToken set to: $authToken"
 
 # login
-curl -s -X POST -d "client_user_session[email]=$EMAIL" -d "client_user_session[password]=$PASSWORD" -d "authenticity_token=$authToken" --cookie session --cookie-jar auth "http://$DOMAIN/user_session" > /dev/null
+curl -s -X POST --data-urlencode "client_user_session[email]=$email" --data-urlencode "client_user_session[password]=$pass" --data-urlencode "authenticity_token=$authToken" --cookie session --cookie-jar auth "http://$domain/user_session" > /dev/null
 if [ ! -f auth ]
   then
-  echo "Unable to log in." 1>&2
+  echo "Unable to log in."
+  exit 1;
 fi
 echo "logged in"
 
 # include --cookie auth on all requests for this session
 
 # get all employer links
-curl -s --cookie auth "http://$DOMAIN/employers" | awk '/employers\/[0-9]+/' | awk -F"\"" '{print $2;}' > emps
+curl -s --cookie auth "http://$domain/employers" | awk '/employers\/[0-9]+/' | awk -F"\"" '{print $2;}' > emps
 if [ ! -f emps ]
   then
   echo "Unable to retrieve list of employers." 1>&2
+  exit 1;
 fi
 echo "Employer listing successful."
 
 # call http://clientapp.changehealthcare.com/employers/123 for each
 for emp in $(cat emps)
 do
-  curl -s --cookie auth "http://$DOMAIN$emp" > /dev/null
+  curl -s --cookie auth "http://$domain$emp" > /dev/null
   echo "Cached employer: $emp"
 done
 

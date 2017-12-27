283d659f87005077e6a6656aadc8295d49bca0c5
Evan Miller <evan_miller1@homedepot.com>
2017-12-27 14:33:45 -0500

    adding more insults

--- utils/insult_generator.rb
+++ utils/insult_generator.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
-adj = %w(lazy stupid insecure idiotic slimy slutty smelly pompous communist dicknose pie-eating racist elitst white-trash drug-loving butterface tone-deaf ugly creepy)
-adv = %w(douche ass turd rectum butt cock shit crotch bitch turd prick slut taint fuck dick boner shart nut sphincter cum)
-nou = %w(pilot canoe captain pirate hammer knob box jockey nazi waffle goblin blossum biscuit clown socket monster hound dragon balloon bucket box can knob)
+adj = %w(lazy stupid insecure idiotic slimy slutty smelly pompous communist dicknose pie-eating racist elitst white-trash drug-loving butterface tone-deaf ugly creepy rusty bastard mangaled shit-spackled witless jealous)
+adv = %w(douche ass turd rectum butt cock shit crotch bitch turd prick slut taint fuck dick boner shart nut sphincter cum queef jizz cunt fart muppet cockalorum brown-nosing douche thundercunt anal scrotum chode nipple fag)
+nou = %w(pilot canoe captain pirate hammer knob box jockey nazi waffle goblin blossum biscuit clown socket monster hound dragon balloon bucket box can knob fucktrumpet shitgibbon wankstain dumpster pouch puddle cockwomble stain lozenge hellbeast thief bedwetter fungus clog cannon)
 
 insults = ARGV[0] ? ARGV[0].to_i : 10
 insults.times{ puts "#{adj.sample} #{adv.sample} #{nou.sample}" }

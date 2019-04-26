7d44ab8648aed486628054c11a8afded09db0219
Evan Miller <evanmiller67@gmail.com>
2019-04-26 08:52:35 -0400

    minimal install

---
+++ bin/simple_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl --output .bash_profile https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.bash_profile
+curl --output .vimrc https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.vimrc
+curl --output .tmux.conf https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.tmux.conf
+curl --output .bashrc https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.bashrc

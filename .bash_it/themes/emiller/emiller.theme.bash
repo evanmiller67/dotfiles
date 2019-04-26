5bbae3462aef6fe6524ef498f3b597f05a80d53a
Evan Miller <evanmiller67@gmail.com>
2019-04-26 10:49:57 -0400

    custom prompt colors

--- .bash_it/themes/emiller/emiller.theme.bash
+++ .bash_it/themes/emiller/emiller.theme.bash
@@ -6,11 +6,14 @@
 if tput setaf 1 &> /dev/null; then
     if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
       MAGENTA=$(tput setaf 9)
-      ORANGE=$(tput setaf 172)
+      #ORANGE=$(tput setaf 172)
+      ORANGE=$(tput setaf 178)
       GREEN=$(tput setaf 190)
+      GREEN=$(tput setaf 12)
       PURPLE=$(tput setaf 141)
       WHITE=$(tput setaf 0)
       LAVENDAR=$(tput setaf 12)
+      BLUE=$(tput setaf 9)
     else
       MAGENTA=$(tput setaf 5)
       ORANGE=$(tput setaf 4)
@@ -18,6 +21,7 @@ if tput setaf 1 &> /dev/null; then
       PURPLE=$(tput setaf 1)
       WHITE=$(tput setaf 7)
       LAVENDAR=$(tput setaf 12)
+      BLUE=$(tput setaf 2)
     fi
     BOLD=$(tput bold)
     RESET=$(tput sgr0)
@@ -27,12 +31,17 @@ else
     GREEN="\033[1;32m"
     PURPLE="\033[1;35m"
     WHITE="\033[1;37m"
+    BLUE="\033[1;34m"
     BOLD=""
     RESET="\033[m"
 fi
 
 function prompt_command() {
-  LEFT="\[${BOLD}${MAGENTA}\]\u \[$WHITE\]@ \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w \[$WHITE\]\[$PURPLE\]$(scm_prompt_info)"
+  #LEFT="\[${BOLD}${MAGENTA}\]\u \[$WHITE\]@ \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w \[$WHITE\]\[$PURPLE\]$(scm_prompt_info)"
+  if [ $USER == 'root' ]; then 
+    BLUE=$(tput setaf 160)
+  fi
+  LEFT="\[${BOLD}${BLUE}\]\u \[$WHITE\]@ \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w \[$WHITE\]\[$PURPLE\]$(scm_prompt_info)"
   BOTTOM="\$ \[$RESET\]"
   PS1="${LEFT} ${BOTTOM}"
 }

a3195529900d5885b2cc7e06147338d1a4c0bdf6
Evan Miller <evanmiller67@gmail.com>
2019-05-09 15:37:20 -0400

    quiet install, add aliases

--- bin/simple_install.sh
+++ bin/simple_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-curl --output .bash_profile https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.bash_profile
-curl --output .vimrc https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.vimrc
-curl --output .tmux.conf https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.tmux.conf
-curl --output .bashrc https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.bashrc
+curl --silent --output .bash_profile https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.bash_profile
+curl --silent --output .bash_aliases https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.bash_aliases
+curl --silent --output .vimrc https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.vimrc
+curl --silent --output .tmux.conf https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.tmux.conf
+curl --silent --output .bashrc https://raw.githubusercontent.com/evanmiller67/dotfiles/master/.bashrc
